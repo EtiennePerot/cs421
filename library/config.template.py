4661096cf37b5be4bad924f0f772febb5e23cd6e
Etienne Perot <etienne@perot.me>
2012-02-24 21:38:42 -0500

    Made config parameters match with what pymysql expects

--- library/config.template.py
+++ library/config.template.py
@@ -1,6 +1,6 @@
 dbConfig = {
 	'host': 'mysql.biringa.com',
-	'username': 'cs421g32',
-	'password': 'Put something here',
-	'database': 'cs421project'
+	'user': 'cs421g32',
+	'passwd': 'Put something here',
+	'db': 'cs421project'
 }

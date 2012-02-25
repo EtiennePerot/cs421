e4d7b7c39bf8fe1642c3a2795562116600c64417
Alassane-Ndiaye <alassane.ndiaye@mail.mcgill.ca>
2012-02-25 18:13:56 -0500

    googlebooks.py now fetches isbn, title, numberof pages, release date from google books

--- googlebooks.py
+++ googlebooks.py
@@ -1,9 +1,12 @@
-import  urllib2, json
+import urllib2, json
 for i in range(10):
-    request = urllib2.Request(url='https://www.googleapis.com/books/v1/volumes?q=intitle:'+str(i) +'&maxResults=40')
+    request = urllib2.Request(url='https://www.googleapis.com/books/v1/volumes?q=intitle:' + str(i) + '&maxResults=40')
     result = urllib2.urlopen(request)
     parsed_result = json.loads(result.read())
     book_list = parsed_result['items']
     for item in book_list:
-        if(item['volumeInfo'].has_key('title')):
-            print item['volumeInfo']['title']
+        if item['volumeInfo'].has_key('title') and item['volumeInfo'].has_key('industryIdentifiers') and item['volumeInfo'].has_key('pageCount'):
+            print 'isbn:' + item['volumeInfo']['industryIdentifiers'][0]['identifier']
+            print 'title:' + item['volumeInfo']['title']
+            print 'pages:' + str(item['volumeInfo']['pageCount'])
+            print 'release date:' + item['volumeInfo']['publishedDate'] + '\n'

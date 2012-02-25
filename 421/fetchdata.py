ad053c8f25b60ee5220a8f7fd9df6b5a532aee58
Fire <Fire@Fire-PC.gateway.2wire.net>
2012-02-25 03:12:35 -0500

    updated fetchdata.py

--- 421/fetchdata.py
+++ 421/fetchdata.py
@@ -1,6 +1,6 @@
 import  urllib2, json
-for i in range(100):
-    request = urllib2.Request(url='https://www.googleapis.com/books/v1/volumes?q=intitle:'+str(i))
+for i in range(10):
+    request = urllib2.Request(url='https://www.googleapis.com/books/v1/volumes?q=intitle:'+str(i) +'&maxResults=40')
     result = urllib2.urlopen(request)
     parsed_result = json.loads(result.read())
     book_list = parsed_result['items']

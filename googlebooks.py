9389249629a0cccf7590f903a60d640b4b82abe5
Alassane-Ndiaye <alassane.ndiaye@mail.mcgill.ca>
2012-02-26 23:52:38 -0500

    Update googlebooks.py

--- googlebooks.py
+++ googlebooks.py
@@ -1,26 +1,27 @@
 import urllib2, json
 import library
-from dateutil.dateutil.parser import parse
+import time
+from library.dateutil.dateutil.parser import parse
 import re
-for i in range(10):
-    request = urllib2.Request(url='https://www.googleapis.com/books/v1/volumes?q=intitle:' + str(i) + '&maxResults=40')
-    result = urllib2.urlopen(request)
-    parsed_result = json.loads(result.read())
-    book_list = parsed_result['items']
-    for item in book_list:
-        if item['volumeInfo'].has_key('title') and item['volumeInfo'].has_key('industryIdentifiers') and item['volumeInfo'].has_key('pageCount'):
-            try:
-                date_value = item['volumeInfo']['publishedDate']
-                year = re.search('\d+', date_value)
-                if year.group(0)<1900:
-                    date_value = str(year)
+for i in range(1000):    
+    request = urllib2.Request(url='https://www.googleapis.com/books/v1/volumes?q=isbn:' + str(i) + '&maxResults=40')
+    time.sleep(10)
+    try:
+        result = urllib2.urlopen(request)
+        book_list = json.loads(result.read())['items']
+        for item in book_list:
+            if item.has_key('volumeInfo') and item['volumeInfo'].has_key('publishedDate')  and item['volumeInfo'].has_key('title') and item['volumeInfo'].has_key('industryIdentifiers') and item['volumeInfo'].has_key('pageCount'):
+                year = re.search('\d+', item['volumeInfo']['publishedDate'])
+                if year.group(0) < 1900:
+                    published_date = year.group(0) + '-01-01'
                 else:
-                    date_value = parse(date_value).strftime('%y-%m-%d')
+                    published_date = parse(item['volumeInfo']['publishedDate']).strftime('%y-%m-%d')                
                 library.items.Book.create(
                     title=item['volumeInfo']['title'],
-                    date=date_value,
-                    isbn=item['volumeInfo']['industryIdentifiers'][0]['identifier'],
+                    date=published_date,
+                    isbn=re.search('\d+', item['volumeInfo']['industryIdentifiers'][0]['identifier']).group(0),
                     pages=item['volumeInfo']['pageCount']
                 )
-            except:
-                pass
+    except Exception as detail:
+        print detail
+        pass

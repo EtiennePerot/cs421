e1cf2408ee87c956b356a894e340239f259edea8
Etienne Perot <etienne@perot.me>
2012-02-28 00:24:04 -0500

    Remove unused imdb module

--- imdb/exceptions.py
+++
@@ -1,8 +0,0 @@
-class ImdbBaseError(Exception):
-    pass
-
-class ImdbInvalidLocale(ImdbBaseError):
-    pass
-
-class ImdbRequestError(ImdbBaseError):
-    pass
--- imdb/test/test_basic.py
+++
@@ -1,9 +0,0 @@
-import imdb
-
-
-def test_simple_search():
-    sr = imdb.search("ghost busters")
-    first = sr['data']['results'][0]['list'][0]
-    print first
-    assert first['title'] == 'Ghost Busters'
-    assert first['tconst'] == 'tt0087332'

3b465a8f764493851df5113c0b3694c113a3e0af
Etienne Perot <etienne@perot.me>
2012-03-17 19:44:50 -0400

    Simplifications, reordering

--- p3_q1_select.py
+++ p3_q1_select.py
@@ -3,7 +3,7 @@ from library import sqlQuery
 print sqlQuery("""
 -- All actors that have acted in 10 movies or more
 -- Useful to know which actors are popular
-SELECT actors.name, COUNT(actors.name) FROM actors
+SELECT actors.name, COUNT(actors.name) AS numberOfMovies FROM actors
 	NATURAL JOIN has_actors
 	GROUP BY actors.name
 	HAVING COUNT(actors.name) >= 10
@@ -23,21 +23,31 @@ SELECT iid, title FROM items NATURAL JOIN video
 """)
 
 print sqlQuery("""
+-- List members who have not given back their items before the end of their reservation date, and are already in bad standing.
+-- Useful when wanting to see the list of people whose membership should probably be ended.
+SELECT members.pnid, name, email, balance FROM members NATURAL JOIN people WHERE standing = 'bad' AND EXISTS (
+	SELECT 1 FROM reserved_by
+	WHERE reserved_by.pnid = members.pnid
+	AND `to` < CURRENT_DATE
+	LIMIT 1 -- Only need one record to check existence
+)
+""")
+
+print sqlQuery("""
 -- Audio albums that have a total duration of more than 10 minutes, combined with videos that are between 8 and 12 minutes in length, ordered by total duration
 -- Useful if we have "x" minutes of time and want to borrow something that is guaranteed to fill that time gap, no matter what type of media it is
 (
 	SELECT
-		'audio' as `type`,
+		'audio' AS `type`,
 		title,
-		ROUND(`SUM(songs.duration)` / 60) AS totalDuration
+		ROUND(longAlbums.totalAlbumDuration / 60) AS totalDuration
 	FROM items NATURAL JOIN (
-		SELECT audio.iid, SUM(songs.duration) FROM audio NATURAL JOIN songs
+		SELECT audio.iid, SUM(songs.duration) AS totalAlbumDuration
+		FROM audio NATURAL JOIN songs
 		GROUP BY audio.iid
 		HAVING SUM(songs.duration) > 10 * 60
 	) AS longAlbums
-)
-UNION
-(
+) UNION (
 	SELECT
 	'video' as `type`,
 	title,
@@ -45,15 +55,4 @@ UNION
 	FROM video NATURAL JOIN items
 	WHERE duration >= 10 AND duration <= 12
 ) ORDER BY totalDuration
-""")
-
-print sqlQuery("""
--- List members who have not given back their items before the end of their reservation date, and are already in bad standing.
--- Useful when wanting to see the list of people whose membership should probably be ended.
-SELECT pnid, balance FROM members WHERE standing = 'bad' AND EXISTS (
-	SELECT 1 FROM reserved_by
-	WHERE reserved_by.pnid = members.pnid
-	AND `to` < CURRENT_DATE
-	LIMIT 1 -- Only need one record to check existence
-)
 """)

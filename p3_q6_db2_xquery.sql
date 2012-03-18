a62bca3409bbd2c6990de513290298406a9d59b2
Etienne Perot <etienne@perot.me>
2012-03-17 23:06:39 -0400

    Fixed first example and added sample xqueries

--- p3_q6_db2_xquery.sql
+++ p3_q6_db2_xquery.sql
@@ -12,7 +12,7 @@ echo Inserting rows...
 
 INSERT INTO items(iid, title, date) VALUES(1, 'Guide to writing', '2006-04-11')
 
-INSERT INTO books(iid, isbn, pages, toc) VALUES(1, 1337, 32, '<toc><chapter num="1" title="INTRODUCTION" page="3"><chapter num="1.a" title="Style" page="3"/><chapter num="1.b" title="Composition and Structure" page="3"/><chapter num="1.c" title="Acknowledgement of Support" page="4"/><chapter num="1.d" title="Abstract" page="4"/></chapter><chapter num="2" title="MANUSCRIPT PREPARATION" page="4"><chapter num="2.a" title="Paper and Duplication" page="4"/><chapter num="2.b" title="Printing" page="4"/><chapter num="2.c" title="Font" page="4"/><chapter num="2.d" title="Margins" page="4"/><chapter num="2.e" title="Pagination" page="5"/><chapter num="2.f" title="Spacing" page="5"/><chapter num="2.g" title="Numbering Schemes" page="5"/><chapter num="2.h" title="Division" page="5"><chapter num="2.h.1" title="Body of Manuscript" page="5"/><chapter num="2.h.2" title="Words and Sentences" page="5"/><chapter num="2.h.3" title="Headings and Subheadings" page="6"/></chapter><chapter num="2.i" title="Tables and Figures" page="6"/><chapter num="2.j" title="Table of Contents Preparation Hint" page="6"/></chapter></toc>')
+INSERT INTO books(iid, isbn, pages, toc) VALUES(1, 1337, 32, '<toc><chapter num="1" name="INTRODUCTION" page="3"><chapter num="1.a" name="Style" page="3"/><chapter num="1.b" name="Composition and Structure" page="3"/><chapter num="1.c" name="Acknowledgement of Support" page="4"/><chapter num="1.d" name="Abstract" page="4"/></chapter><chapter num="2" name="MANUSCRIPT PREPARATION" page="4"><chapter num="2.a" name="Paper and Duplication" page="4"/><chapter num="2.b" name="Printing" page="4"/><chapter num="2.c" name="Font" page="4"/><chapter num="2.d" name="Margins" page="4"/><chapter num="2.e" name="Pagination" page="5"/><chapter num="2.f" name="Spacing" page="5"/><chapter num="2.g" name="Numbering Schemes" page="5"/><chapter num="2.h" name="Division" page="5"><chapter num="2.h.1" name="Body of Manuscript" page="5"/><chapter num="2.h.2" name="Words and Sentences" page="5"/><chapter num="2.h.3" name="Headings and Subheadings" page="6"/></chapter><chapter num="2.i" name="Tables and Figures" page="6"/><chapter num="2.j" name="Table of Contents Preparation Hint" page="6"/></chapter></toc>')
 
 INSERT INTO items(iid, title, date) VALUES(2, 'Non-Natural Amino Acids', '2009-06-21')
 
@@ -25,3 +25,11 @@ INSERT INTO books(iid, isbn, pages, toc) VALUES(3, 6542, 994, '<toc><chapter num
 INSERT INTO items(iid, title, date) VALUES(4, 'Algorithm Design', '2009-02-20')
 
 INSERT INTO books(iid, isbn, pages, toc) VALUES(4, 879451, 432, '<toc><chapter num="I" name="Introduction: Some Representative Problems" page="1"><chapter num="I.1" name="A First Problem: Stable Matching" page="12"/><chapter num="I.2" name="Five Representative Problems" page="19"/><chapter num="I.3" name="Solved Exercises" page="22"/><chapter num="I.4" name="Notes and Further Reading" page="28"/></chapter><chapter num="II" name="Basics of Algorithm Analysis" page="29"><chapter num="II.1" name="Computational Tractability" page="29"/><chapter num="II.2" name="Asymptotic Order of Growth" page="35"/><chapter num="II.3" name="Implementing the Stable Matching Algorithm Using Lists and Arrays" page="42"/><chapter num="II.4" name="A Survey of Common Running Times" page="47"/><chapter num="II.5" name="A More Complex Data Structure: Priority Queues" page="57"/><chapter num="II.6" name="Solved Exercises" page="65"/><chapter num="II.7" name="Exercises" page="67"/><chapter num="II.8" name="Notes and Further Reading" page="70"/></chapter></toc>')
+
+echo Executing xquery example 1
+
+XQUERY <titles> { for $t in db2-fn:xmlcolumn('BOOKS.TOC')/toc/chapter/chapter/chapter return <title> { data($t/@name) } </title> } </titles>
+
+echo Executing xquery example 2
+
+XQUERY <chapterlist> { for $c in db2-fn:xmlcolumn('BOOKS.TOC')/toc//chapter where fn:count($c//*) > 5 return <bigchapter subchapters="{ fn:count($c//*) }"> { $c } </bigchapter> } </chapterlist>

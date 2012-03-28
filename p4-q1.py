fe64012895c81066f95b66db1b441d1f44e56500
Mona Ghassemi <monagz@gmail.com>
2012-03-28 10:08:08 -0400

    stored procedure: getting there, not quite there yet...

--- p4-q1.py
+++ p4-q1.py
@@ -20,23 +20,37 @@ sqlQuery(
 
 CREATE PROCEDURE procname(IN fine INTEGER)
 BEGIN
+REPEAT
+ DECLARE number INTEGER DEFAULT 0; 
+ SELECT pnid INTO number 
+  FROM members NATURAL JOIN reserved_by 
+  WHERE type = "borrowed" AND 'to' < CURRENT_DATE LIMIT 1;
+ UPDATE members
+  SET balance = balance + 1*(CURRENT_DATE - `to`) WHERE pnid = number;
+UNTIL number = 0
+END REPEAT;
+END
 
 #users with overdue items
-SELECT * FROM members NATURAL JOIN reserved_by WHERE type = "borrowed" AND 'to' < CURRENT_DATE LIMIT 30;
-#add fine to their accounts
-UPDATE members 
-SET balance = balance + 1*(CURRENT_DATE - 'to')
-  WHERE pnid IN 
-(SELECT * FROM members NATURAL JOIN reserved_by WHERE type = "borrowed" AND 'to' < CURRENT_DATE);
+#add fine to their accounts (DOES NOT QUITE WORK DUE TO STUPID ERROR)
+
+#UPDATE members 
+#SET balance = balance + 1*(CURRENT_DATE - `to`)
+#  WHERE pnid =
+#(SELECT pnid FROM members 
+# WHERE pnid IN 
+# (SELECT * FROM members NATURAL JOIN reserved_by 
+#  WHERE type = "borrowed" AND 'to' < CURRENT_DATE));
 
 
-
-
-END
 
 
 )
 
+
+#while exists record that matches
+#set somevaraible to id of record that matches
+#delete record with that id
 
 
 c.callproc(procname, args)

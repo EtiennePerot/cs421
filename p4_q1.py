a312078794822c07abacd1de6b8c61af64467bdd
Etienne Perot <etienne@perot.me>
2012-03-29 02:57:00 -0400

    Finished q1

--- p4_q1.py
+++ p4_q1.py
@@ -1,9 +1,46 @@
-#from library import sqlQuery #not using this
+from library import sqlQuery
 
-import library import base
+from library import *
 
+try: # Drop procedure if it exists
+	sqlQuery('DROP PROCEDURE addFines')
+except: # It didn't exist
+	pass
+# Now define it
+sqlQuery("""
+	CREATE PROCEDURE addFines(IN fine INTEGER) BEGIN
+		DECLARE myPnid INT;
+		DECLARE myDate DATE;
+		DECLARE done INT DEFAULT FALSE;
+		DECLARE pnids CURSOR FOR SELECT pnid, `to` FROM members NATURAL JOIN reserved_by WHERE type = "borrowed" AND `to` < CURRENT_DATE;
+		DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+		OPEN pnids;
+		read_loop: LOOP
+			FETCH pnids INTO myPnid, myDate;
+			IF done THEN
+				LEAVE read_loop;
+			END IF;
+			IF EXISTS(SELECT 1 FROM employees WHERE pnid = myPnid) THEN
+				UPDATE employees SET salary = salary - fine * (CURRENT_DATE - myDate) WHERE pnid = myPnid LIMIT 1;
+			ELSE
+				UPDATE members SET balance = balance + fine * (CURRENT_DATE - myDate) WHERE pnid = myPnid LIMIT 1;
+			END IF;
+		END LOOP;
+		CLOSE pnids;
+	END
+""")
+
+
+fine = None
+while fine is None:
+	try:
+		fine = int(raw_input('How much is the fine? '))
+	except:
+		print 'Please enter an integer.'
+		fine = None
+
+transactionStart()
 c = makeCursor()
-
-#write procedure (temp name procname) here
-
-c.callproc(procname, args)
+c.callproc('addFines', [fine])
+transactionCommit()
+print 'Done.'
